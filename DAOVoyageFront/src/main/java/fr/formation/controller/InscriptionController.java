a83c15dcffc509f94b2e2867d93f5e5c012a28df
Antrootz <toto-smx@hotmail.fr>
2020-05-07 14:31:08 +0200

    Layout + controllers
    inscription et panier

--- DAOVoyageFront/src/main/java/fr/formation/controller/InscriptionController.java
+++ DAOVoyageFront/src/main/java/fr/formation/controller/InscriptionController.java
@@ -1,5 +1,61 @@
 package fr.formation.controller;
 
-public class InscriptionController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import fr.formation.model.Client;
+import fr.formation.model.Site;
+import fr.formation.model.Utilisateur;
+
+@Controller
+public class InscriptionController {
+	
+	@Autowired
+	Site site;
+	
+	@GetMapping("/inscription")
+	public String getInscription() {
+		return "inscription";
+	}
+	
+	@PostMapping("/inscription")
+	public String postInscription(
+			@RequestParam String adresseMail,
+			@RequestParam String login,
+			@RequestParam String password, Model model, HttpSession session) {
+		
+		Utilisateur u = site.checkMail(adresseMail);
+		if (u == null) {
+			u = new Client(login, password, adresseMail);
+			site.inscription(u);
+			session.setAttribute("isConnect", null);
+			return "accueil";	
+		} else {
+			session.setAttribute("isConnect", "N");
+			return getInscription();
+		}
+	}
+	
+	@PostMapping("/inscription/checkMail")
+	public String checkMail(@RequestParam(required=false) String adresseMail, HttpSession session) {
+		if (adresseMail !=  "" && adresseMail != null) {
+			
+		}
+		Utilisateur u = site.checkMail(adresseMail);
+		if (u == null) {
+			session.setAttribute("mailCorrect", "Y");
+			return "accueil";	
+		} else {
+			session.setAttribute("mailCorrect", "N");
+			return getInscription();
+		}
+	}
+	
+	
 }
--- DAOVoyageFront/src/main/java/fr/formation/controller/PanierController.java
+++ DAOVoyageFront/src/main/java/fr/formation/controller/PanierController.java
@@ -1,5 +1,68 @@
 package fr.formation.controller;
 
-public class PanierController {
+import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import fr.formation.model.Client;
+import fr.formation.model.Site;
+import fr.formation.model.Transport;
+import fr.formation.model.Ville;
+import fr.formation.model.Voyage;
+
+@Controller
+public class PanierController {
+	
+	@Autowired
+	Site site;
+	
+	@GetMapping("/panier")
+	public String getPanier(HttpSession session) {
+		session.setAttribute("panier",  site.getPanier());
+		double total=0;
+		for(Voyage voy : site.getPanier())
+		{
+			total+=voy.getPrix();
+		}
+		session.setAttribute("total", Math.floor(total*100)/100);
+		return "panier";
+	}
+	
+	@PostMapping("/panier/paiement")
+	public String postPanier(Model model, HttpSession session) {
+		
+		List<Voyage> commande = site.getPanier();
+		for(Voyage v : commande)
+		{
+		Ville v1 = v.getV1();
+		Ville v2 = v.getV2();
+		Transport t = v.getT();
+		
+		Voyage voyage = new Voyage(v1,v2,t);
+		voyage.setStatut("Commande");
+		
+		//int id = (int) request.getSession().getAttribute("id");
+		//Client client = (Client) Site.getInstance().getDaoUtilisateur().selectById(id);
+		Client client = (Client) session.getAttribute("client");
+        voyage.setClient(client);
+        
+		site.getDaoVoyage().save(voyage);
+		}
+		
+		site.getPanier().clear();
+		return "redirect:/panier";
+	}
+
+	@PostMapping("/panier/remove")
+	public String removeById(@RequestParam int nb) {
+		site.getPanier().remove(nb);
+		return "done";
+	}
 }
