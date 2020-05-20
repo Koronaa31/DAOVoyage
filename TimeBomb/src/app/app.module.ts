ddff3f2999724f2a273617ed7fc4d48661843da2
Antrootz <toto-smx@hotmail.fr>
2020-05-20 12:28:05 +0200

    classes + components + services

--- TimeBomb/src/app/app.module.ts
+++ TimeBomb/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LobbyComponent } from './lobby/lobby.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LobbyComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule
