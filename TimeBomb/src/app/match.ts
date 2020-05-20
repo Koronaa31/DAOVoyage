42710ccf27978eb81d89c133532e34174065be01
Antrootz <toto-smx@hotmail.fr>
2020-05-20 16:15:51 +0200

    jquery

--- TimeBomb/src/app/match.ts
+++ TimeBomb/src/app/match.ts
@@ -1,12 +1,15 @@
+import { User } from './user';
+import { Card } from './card';
+
 export class Match {
 
-  constructor (public id?,
-               public name?,
-               public size?,
-               public state?,
-               public winner?,
-               public owner?,
-               public current?,
-               public players?,
-               public deck?) {  };
+  constructor (public id?: number,
+               public name?: string,
+               public size?: number,
+               public state?: string,
+               public winner?: string,
+               public owner?: User,
+               public current?: User,
+               public players?: Array<User>,
+               public deck?: Array<Card>) {  }
 }
